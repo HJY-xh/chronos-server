2454b453f7ce70960bd40bd2e3040ff0aa70f028
黄建宇 <huangjianyu@lightinthebox.com>
2021-05-24 17:20:03 +0800

    http升级为https

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ const koaBody = require('koa-body');
 const Config = require('./utils/config');
 const connectDB = require('./utils/db');
 const routes = require('./routes/index');
+const https = require('https');
+const fs = require('fs');
+const enforceHttps = require('koa-sslify');
 
 connectDB();
 
 const app = new Koa();
+
+// 强制转化http请求为https
+app.use(enforceHttps());
 
 app.use(koaBody());
 
@@ -16,6 +22,11 @@ process.on('uncaughtException', (err) => {
 	console.log(err);
 });
 
-app.listen(Config.port, () => {
+const options = {
+	key: fs.readFileSync('../ssl/xh-hjy.top.key'),
+	cert: fs.readFileSync('../ssl/xh-hjy.top.pem')
+};
+
+https.createServer(options, app.callback()).listen(Config.port, () => {
 	console.log(`app start at ${Config.port}`);
 });
